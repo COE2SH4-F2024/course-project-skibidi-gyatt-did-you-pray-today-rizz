bb9c98389490d04194da514de0ebd43ac3f48efa
plassers <plassers@mcmaster.ca>
2024-12-02 20:10:48 -0500

    Did dev1 part, and tested it with dev2 for proper implementation

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -1,76 +1,72 @@
 #include "GameMechs.h"
+#include "MacUILib.h"
+#include "Player.h"
 
+// Default constructor: initializes default values for game mechanics
 GameMechs::GameMechs()
 {
-    
+    input = 0;
+    exitFlag = false;
+    loseFlag = false;
+    score = 0;
+    boardSizeX = 30;
+    boardSizeY = 15;
+    objPos food; // Placeholder for food position
 }
 
+// Specialized constructor: allows custom board size initialization
 GameMechs::GameMechs(int boardX, int boardY)
 {
-    
+    input = 0;
+    exitFlag = false;
+    loseFlag = false;
+    score = 0;
+    boardSizeX = boardX;
+    boardSizeY = boardY;
+    objPos food; // Placeholder for food position
 }
 
-// do you need a destructor?
-GameMechs::~GameMechs()
+// Returns the current status of the exit flag
+bool GameMechs::getExitFlagStatus()
 {
-    
+    return exitFlag;
 }
 
-bool GameMechs::getExitFlagStatus() const
+// Captures and returns the current player input if available
+char GameMechs::getInput()
 {
-
+    if (MacUILib_hasChar() == 1) {
+        input = MacUILib_getChar();
+    }
+    return input;
 }
 
-bool GameMechs::getLoseFlagStatus() const
+// Returns the board's width
+int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
-    
 
-char GameMechs::getInput() const
+// Returns the board's height
+int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
-int GameMechs::getScore() const
-{
-
-}
-
-void GameMechs::incrementScore()
-{
-    
-}
-
-int GameMechs::getBoardSizeX() const
-{
-
-}
-
-int GameMechs::getBoardSizeY() const
-{
-
-}
-
-
+// Sets the exit flag to true, indicating the game should terminate
 void GameMechs::setExitTrue()
 {
-
+    exitFlag = true;
 }
 
-void GameMechs::setLoseFlag()
-{
-    
-}
-
+// Updates the stored input with the provided character
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input;
 }
 
+// Clears the stored input, resetting it to 0
 void GameMechs::clearInput()
 {
-
+    input = 0;
 }
-
-// More methods should be added here
